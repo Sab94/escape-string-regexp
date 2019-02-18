79c8e127570f105e150312a866d16701dfcc4df7
Sabyasachi Patra <asabya@gmail.com>
2019-02-19 00:57:49 +0530

    added escapeStringRegexp

---
+++ mod.ts
@@ -0,0 +1,9 @@
+const matchOperatorsRe = /[|\\{}()[\]^$+*?.]/g;
+
+export function escapeStringRegexp(str) {
+	if (typeof str !== 'string') {
+		throw new TypeError('Expected a string');
+	}
+
+	return str.replace(matchOperatorsRe, '\\$&');
+};
